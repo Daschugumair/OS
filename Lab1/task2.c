6c64bff3a5b7cb3ce958dcb6bed11b133a87140f
Fabian Schrammel <fabianschrammel@gmail.com>
2016-11-11 16:52:31 +0100

    2: Code style, my_value global and more sleep

--- Lab1/task2.c
+++ Lab1/task2.c
@@ -1,65 +1,74 @@
+// vim: set cc=100 ts=4 tw=99 sts=4 et:
 #include<stdio.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
 
-/**
-    QUESTION
+int my_value = 42;
 
-    In the task it says we should simply change the task1 to use pthread instead of fork.
-    However it is not specified how the memory of my_value will be shared. I simply passed the pointer here.
-    Obviously changing the value in the sub thread changes the value in the main thread. But it is not clear to me if this is what is intended.
-
-*/
-
-void* thread_function(int* my_value)
+void* thread_function()
 {
-    *my_value = 18951;
+    // Sleep for 150ms
+    int sleep_err = usleep(150000);
+    if (sleep_err == -1)
+    {
+        fprintf(stderr, "Childs sleeping after creation failed with %i", sleep_err);
+        exit(1);
+    }
+
+    // Change value and print
+    my_value = 18951;
     fprintf(stderr, "Hello I am the sub thread\n"
                     "\tMy pid is %d\n"
                     "\tmy_value is %i\n",
-                    getpid(), *my_value);
+                    getpid(), my_value);
 
-    usleep(500000);
+    // Cleep for additional 500ms
+    sleep_err = usleep(500000);
+    if (sleep_err == -1)
+    {
+        fprintf(stderr, "Child sleeping failed with %i", sleep_err);
+        exit(1);
+    }
 
-    //int *ret = malloc(sizeof(int));
-    //*ret = 5;
-    //pthread_exit((void*)ret);
     pthread_exit((void*)NULL);
 }
 
 int main()
 {
-    int my_value = 42;
     pthread_t thread;
     int *ret;
 
-    int err1 = pthread_create(&thread, NULL, (void *)&thread_function, &my_value);
-    if (err1 != 0) {
+    // Create child thread
+    int err = pthread_create(&thread, NULL, (void *)&thread_function, &my_value);
+    if (err != 0)
+    {
         printf("Error!");
         return -1;
     }
-
     fprintf(stderr, "sub thread created\n");
 
-    usleep(150000);
+    // Sleep for 150ms
+    int sleep_err = usleep(150000);
+    if (sleep_err == -1)
+    {
+        fprintf(stderr, "Parents sleeping after child creation failed with %i", sleep_err);
+        exit(1);
+    }
 
     fprintf(stderr, "Hello I am the main thread\n"
                     "\tMy pid is %d\n"
                     "\tmy_value is %i\n",
                     getpid(), my_value);
 
-    int err2 = pthread_join(thread,(void**)&ret);
-
-    if (err2 != 0) {
+    // Wait for child thread and exit
+    err = pthread_join(thread,(void**)&ret);
+    if (err != 0)
+    {
         printf("Error!");
         return -1;
     }
-
     fprintf(stderr, "sub thread terminated\n");
-
-    //printf("ret is %i\n", *ret);
-    //free(y);
 
     return 0;
 }
