dff48b25bb061413372d4fb56ff421a840fe7588
tbjoe91 <johannes.roth91@googlemail.com>
2016-11-11 13:47:10 +0100

    task1 and task2 completed and answered in Q1.txt and Q2.txt

--- Lab1/task1.c
+++ Lab1/task1.c
@@ -7,14 +7,24 @@ int main()
     int my_value = 42;
     int status;
     int pid;
+
     pid=fork();
     usleep(150000);
-    if(pid<0)
+
+    // Something went wrong
+    if(pid < 0)
     {
         fprintf(stderr,"Error\n");
         exit(1);
     }
-    else if(pid==0)
+
+    // Parent process announces the creation
+    if (pid != 0) {
+        fprintf(stderr, "Child Process with pid %i created\n", pid);
+    }
+
+    // Child process prints values
+    if(pid == 0)
     {
         my_value = 18951;
         fprintf(stderr, "Hello I am the child process\n"
@@ -24,13 +34,26 @@ int main()
         usleep(500000);
         exit(0);
     }
-    else
-    {
-        wait(&status);
-        fprintf(stderr, "Hello I am the parent process\n"
-                        "\tMy pid is %d\n"
-                        "\tmy_value is %i\n",
-                        getpid(), my_value);
-        exit(0);
-    }
+    
+    // Parent process prints values
+    fprintf(stderr, "Hello I am the parent process\n"
+                    "\tMy pid is %d\n"
+                    "\tmy_value is %i\n",
+                    getpid(), my_value);
+
+
+    // wait for the child and terminate
+    wait(&status);
+    fprintf(stderr, "Child Process with pid %i terminated\n", pid);
+
+    /**
+        QUESTION
+
+        In the task it says:
+        Both the parent and child have to print my_value along with the message that states their PID and the parent has to print it in the child termination message as well.
+
+        I am not sure what is actually meant. Should I print everything again for the parent here?
+    */
+
+    return 0;
 }
--- Lab1/task2.c
+++ Lab1/task2.c
@@ -3,14 +3,23 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+/**
+    QUESTION
 
-void* thread_function(void)
+    In the task it says we should simply change the task1 to use pthread instead of fork.
+    However it is not specified how the memory of my_value will be shared. I simply passed the pointer here.
+    Obviously changing the value in the sub thread changes the value in the main thread. But it is not clear to me if this is what is intended.
+
+*/
+
+void* thread_function(int* my_value)
 {
-    fprintf(stderr, "Hello I am a thread\n"
-                    "\tMy pid is %d\n",
-                    getpid());
-    //my_value = 18951;
-    //fprintf(stderr, "my_value is %i\n", my_value);
+    *my_value = 18951;
+    fprintf(stderr, "Hello I am the sub thread\n"
+                    "\tMy pid is %d\n"
+                    "\tmy_value is %i\n",
+                    getpid(), *my_value);
+
     usleep(500000);
 
     //int *ret = malloc(sizeof(int));
@@ -25,20 +34,29 @@ int main()
     pthread_t thread;
     int *ret;
 
-    int err1 = pthread_create(&thread, NULL, (void *)&thread_function, NULL);
+    int err1 = pthread_create(&thread, NULL, (void *)&thread_function, &my_value);
+    if (err1 != 0) {
+        printf("Error!");
+        return -1;
+    }
+
+    fprintf(stderr, "sub thread created\n");
+
     usleep(150000);
 
-    fprintf(stderr, "Hello I am the parent process\n"
+    fprintf(stderr, "Hello I am the main thread\n"
                     "\tMy pid is %d\n"
                     "\tmy_value is %i\n",
                     getpid(), my_value);
 
     int err2 = pthread_join(thread,(void**)&ret);
 
-    if (err1 != 0 || err2 != 0) {
+    if (err2 != 0) {
         printf("Error!");
         return -1;
     }
+
+    fprintf(stderr, "sub thread terminated\n");
 
     //printf("ret is %i\n", *ret);
     //free(y);
